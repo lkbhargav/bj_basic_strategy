220cd8b66ff112f95a1e72180dbdbf6960083340
Bhargav Lakkur Kusha Kumar <bhargav@Bhargavs-MacBook-Pro.local>
2025-01-08 13:40:16 -0500

    Added some test cases for the insurance bet

--- src/insurance.rs
+++ src/insurance.rs
@@ -6,3 +6,24 @@ pub fn should_i_take_insurance(
 ) -> bool {
     return dealer_up_card == 11 && cards_in_hand.len() == 2 && true_count >= 3;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_take_insurance() {
+        assert!(should_i_take_insurance(&vec![9, 2], 11, 3));
+        assert!(should_i_take_insurance(&vec![10, 10], 11, 5));
+        assert!(should_i_take_insurance(&vec![2, 2], 11, 4));
+    }
+
+    #[test]
+    fn should_not_take_insurance() {
+        assert!(!should_i_take_insurance(&vec![9, 2], 11, 1));
+        assert!(!should_i_take_insurance(&vec![10, 10], 11, 2));
+        assert!(!should_i_take_insurance(&vec![2, 2], 11, 1));
+
+        assert!(!should_i_take_insurance(&vec![10, 10], 10, 5));
+    }
+}
